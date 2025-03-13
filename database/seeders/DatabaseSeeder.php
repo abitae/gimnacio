7bd84ac297f35912ee92baadb890389cdbfb0820
Abel Arana <abel.arana@hotmail.com>
2025-03-13 17:54:56 -0500

    Actualizar datos de usuario en el seeder y reorganizar la estructura del encabezado en la vista principal.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Abel Arana',
+            'email' => 'abel.arana@hotmail.com',
+            'password' => bcrypt('lobomalo123'),
         ]);
     }
 }
